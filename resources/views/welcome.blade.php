154a29599c2e54eb4dabc68cac5c1c86833b9274
nyosru <nyos@rambler.ru>
2026-04-02 20:06:16 +0500

    ++ ci/cd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
             </style>
         @endif
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+    <body class=" flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
 
 laravel  тест задание API логистик, свагер кликать тут >> <a href="/api/documentation">swagger</a>
     </body>
